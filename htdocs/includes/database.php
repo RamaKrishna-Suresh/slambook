95490ba00180c1455f16ccc1d37da45d8cf8a5b9
Ramakrishna Suresh <ramakrishnasuresh@Ramakrishnas-MacBook-Pro.local>
2019-04-21 23:57:42 -0300

    added debug function

--- htdocs/includes/database.php
+++ htdocs/includes/database.php
@@ -1,15 +1,13 @@
 <?php
 include_once("config.php");
+include_once("queries.php");
+
 $dbh = null;
-try
-{
+try {
     $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
+} catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
-
 
 /*
  * Works for both insert and update
@@ -21,8 +19,30 @@ function save($sql, $params) {
     foreach ($params as $param_name => $param_value) {
         $query->bindParam($param_name, $param_value);
     }
+    echo $query->queryString;
+    $query->execute();
 
     return PDOStatement::rowCount();
+}
+
+function fetch($sql, $params) {
+    global $dbh;
+    $query= $dbh->prepare($sql);
+
+    foreach ($params as $param_name => $param_value) {
+        $query->bindParam($param_name, $param_value);
+    }
+    debug_to_console($query->queryString);
+
+    $query->execute();
+    return $query->fetchAll();
+}
+
+function debug_to_console( $data ) {
+    $output = $data;
+    if ( is_array( $output ) )
+        $output = implode( ',', $output);
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
 }
 
 ?>
